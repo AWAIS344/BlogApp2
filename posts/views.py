28b73cac6ba583a755d48ed18836b40263dc277c
awais3344 <awaisalishah02@gmail.com>
2025-03-29 11:55:26 +0500

    Blog Web App

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def Home(request):
+posts=[
+    {
+        "id":0,"name":"Django","content":"Django is very powerful Python Based framework"
+    },
+    {
+        "id":1,"name":"React","content":"React is very powerful Js Based framework"
+    },
+    {
+        "id":2,"name":"Node Js","content":"Node Js is very powerful Js framework widely used"
+    },
+]
 
+
+
+
+
+def Home(request):
+    
+    # context={"posts":posts}
     return render(request , "index.html")
