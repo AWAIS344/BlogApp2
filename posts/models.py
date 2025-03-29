1da4dd3971ad8f2a2f78eaffd3b01ec4c20348fd
awais3344 <awaisalishah02@gmail.com>
2025-03-29 13:39:39 +0500

    Blog Web App

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+
+    post_title=models.CharField(max_length=60)
+    post_content=models.TextField()
+    updated_at =models.DateTimeField(auto_now=True)
