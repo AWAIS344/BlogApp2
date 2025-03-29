7f87e16006ec9a28f630944dcaf3b4cb51a23799
awais3344 <awaisalishah02@gmail.com>
2025-03-29 22:36:39 +0500

    Blog Web App

--- posts/admin.py
+++ posts/admin.py
@@ -5,9 +5,10 @@ from .models import Posts
 
 
 
+@admin.register(Posts)   #if used this dont use code at line 14
 class PostAdmin(admin.ModelAdmin):
     list_display=["id","post_title","post_content"]
 
 
 
-admin.site.register(Posts, PostAdmin)  #we ca
+admin.site.register(Posts, PostAdmin)  #we can use code at line 7 except this line
