69e250f63d0c23cc702b7c32050c31daf90320f4
awais3344 <awaisalishah02@gmail.com>
2025-03-29 22:35:25 +0500

    Blog Web App

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Posts
 # Register your models here.
+
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=["id","post_title","post_content"]
+
+
+
+admin.site.register(Posts, PostAdmin)  #we ca
